d28132d791304d95d636fc795909b120c9a211ef
Andrew <andrewplesniak@gmial.com>
2019-09-08 21:37:44 -0400

    redesign of styling and updated for masters degree interests

--- js/grayscale.js
+++ js/grayscale.js
@@ -32,7 +32,7 @@ $('.navbar-collapse ul li a').click(function() {
     $(this).closest('.collapse').collapse('toggle');
 });
 
-// Google Maps Scripts
+/*// Google Maps Scripts
 var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
@@ -183,3 +183,4 @@ function init() {
         icon: image
     });
 }
+*/
